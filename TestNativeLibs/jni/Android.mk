31de5c5bbe76476e70696a3363434b7a0e50855f
Reynaldo H. Verdejo Pinochet <reynaldo@collabora.com>
2013-09-02 20:36:47 -0400

    TestNativelibs: move to gupnp 0.20.5

--- TestNativeLibs/jni/Android.mk
+++ TestNativeLibs/jni/Android.mk
@@ -104,7 +104,7 @@ include $(CLEAR_VARS)
 MY_GLIB := glib-2.34.3
 MY_LIBSOUP := libsoup-2.40.3
 MY_GSSDP := gssdp-0.14.4
-MY_GUPNP := gupnp-0.20.3
+MY_GUPNP := gupnp-0.20.5
 MY_GUPNPAV := gupnp-av-0.12.1
 
 LOCAL_MODULE := dleyna-jni
