8df1907076d664c79792ca6c2bd4432db51d7281
Tom Keel <thomas.keel@intel.com>
2013-09-13 11:28:13 +0200

    DLeynaDemo: Add APP_PLATFORM to Application.mk
    APP_PLATFORM happened to default to what I wanted for some
    undetermined reason, but it should be set explicitly.
    
    Also, set APP_ABI to build for both ARM and x86.

--- DLeynaDemo/jni/Application.mk
+++ DLeynaDemo/jni/Application.mk
@@ -1,2 +1,16 @@
-#APP_ABI := armeabi x86
-APP_ABI := x86
+# Application Binary Interface.
+# Set to 'armeabi' or 'x86' or both
+APP_ABI := armeabi x86
+
+# Android Platform, aka API Level.
+# Set to one of the following:
+#   android-3  <=> Android 1.5
+#   android-4  <=> Android 1.6
+#   android-5  <=> Android 2.0
+#   android-6  <=> Android 2.0.1
+#   android-7  <=> Android 2.1
+#   android-8  <=> Android 2.2
+#   android-9  <=> Android 2.3
+#   android-14 <=> Android 4.0
+#   android-18 <=> Android 4.3
+APP_PLATFORM := android-9
